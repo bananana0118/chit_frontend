713b391185b5016a5a800ac602059490588f9ebd
bananana0118 <bananana0118@gmail.com>
2024-11-28 19:13:56 +0900

    feat : 버튼 최소너비 반응형처리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
           </p>
           <ChitLogo width={160} height={78}></ChitLogo>
         </section>
-        <div className="px- flex w-full flex-row items-center justify-center rounded-md bg-[#03C75A] p-[14px] text-white">
+        <div className="px- button-container flex w-full flex-row items-center justify-center rounded-md bg-[#03C75A] p-[14px] text-white">
           <NaverLogo width={18} height={18}></NaverLogo>
           <div className="text-medium-large ml-3">
             로그인하고 3초만에 시참 생성하기
