19b77e670b4bffd050a6193152234e152ecee37b
bananana0118 <bananana0118@gmail.com>
2025-02-14 18:41:29 +0900

    feat : 스트리머 툴 컴포넌트 함수화 및 props 설정정

--- src/app/components/molecules/StreamerTools.tsx
+++ src/app/components/molecules/StreamerTools.tsx
@@ -7,13 +7,20 @@ import BtnSetting from '../atoms/button/BtnSetting';
 export default function StreamerTools({
   sessionCode,
   channelId,
+  isSessionOn,
+  onClickSessionHandler,
 }: {
   sessionCode: string;
   channelId: string;
+  isSessionOn: boolean;
+  onClickSessionHandler: () => void;
 }) {
   return (
     <div className="flex justify-between">
-      <BtnOnOffParticiation isOn={true} />
+      <BtnOnOffParticiation
+        isSessionOn={isSessionOn}
+        onClickSessionHandler={onClickSessionHandler}
+      />
       <div className="flex flex-row rounded-md bg-background-sub p-2">
         <BtnCopyLink
           link={`${process.env.NEXT_PUBLIC_FRONT_API_URL}/${channelId}/${sessionCode}`}
