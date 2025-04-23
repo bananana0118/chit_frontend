451d70492fce35593b040cd991d03791aa431952
bananana0118 <bananana0118@gmail.com>
2025-04-23 21:41:58 +0900

    [build]: 빌드 에러 해결

--- src/services/_axios/sessionClient.ts
+++ src/services/_axios/sessionClient.ts
@@ -1,7 +1,8 @@
 import axios, { AxiosError, AxiosInstance, AxiosResponse, InternalAxiosRequestConfig } from 'axios';
-import { ErrorResponse } from '../streamer/type';
-import SessionError, { SessionErrorCode } from '@/errors/sessionError';
+
 import setUpTokenInterceptor from './setUpTokenInterceptor';
+import SessionError, { SessionErrorCode } from '@/errors/sessionError';
+import { ErrorResponse } from '../streamer/type';
 
 const sessionClient: AxiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL, // API의 기본 URL
@@ -32,54 +33,54 @@ sessionClient.interceptors.request.use(
 );
 setUpTokenInterceptor(sessionClient);
 
-// // 응답 인터셉터
-// sessionClient.interceptors.response.use(
-//   (response: AxiosResponse) => {
-//     return response;
-//   },
-//   (error: AxiosError<ErrorResponse>) => {
-//     // 요청 오류 처리
-//     if (error && error.response) {
-//       const { status, error: message } = error.response?.data;
-//       if (status === 400) {
-//         if (message === '현재 진행중인 라이브 방송을 찾을 수 없습니다. 다시 확인해 주세요.') {
-//           throw new SessionError(SessionErrorCode.LIVE_SESSION_NOT_FOUND);
-//         }
-//         if (message === '이미 진행 중인 컨텐츠 세션이 존재합니다. 중복 생성을 할 수 없습니다.') {
-//           throw new SessionError(SessionErrorCode.LIVE_SESSION_EXISTS);
-//         }
-//         if (message === '현재 진행 중인 시청자 참여 세션이 없습니다. 다시 확인해 주세요.') {
-//           throw new SessionError(SessionErrorCode.SESSION_CLOSED);
-//         }
-//       }
-//     } else if (axios.isAxiosError(error)) {
-//       if (error.response) {
-//         // 서버에서 응답한 에러
-//         console.warn('🚨 Server Response:', error.response.data);
-//         return {
-//           status: error.response.status,
-//           error: error.response.data?.error || '서버 오류가 발생했습니다.',
-//           data: error.response.data?.data,
-//         };
-//       } else if (error.request) {
-//         // 요청이 전송되었지만 응답이 없음
-//         return {
-//           status: 503,
-//           error: '서버 응답이 없습니다. 네트워크 상태를 확인하세요.',
-//           data: 'null',
-//         };
-//       }
-//     }
+// 응답 인터셉터
+sessionClient.interceptors.response.use(
+  (response: AxiosResponse) => {
+    return response;
+  },
+  (error: AxiosError<ErrorResponse>) => {
+    // 요청 오류 처리
+    if (error && error.response) {
+      const { status, error: message } = error.response?.data;
+      if (status === 400) {
+        if (message === '현재 진행중인 라이브 방송을 찾을 수 없습니다. 다시 확인해 주세요.') {
+          throw new SessionError(SessionErrorCode.LIVE_SESSION_NOT_FOUND);
+        }
+        if (message === '이미 진행 중인 컨텐츠 세션이 존재합니다. 중복 생성을 할 수 없습니다.') {
+          throw new SessionError(SessionErrorCode.LIVE_SESSION_EXISTS);
+        }
+        if (message === '현재 진행 중인 시청자 참여 세션이 없습니다. 다시 확인해 주세요.') {
+          throw new SessionError(SessionErrorCode.SESSION_CLOSED);
+        }
+      }
+    } else if (axios.isAxiosError(error)) {
+      if (error.response) {
+        // 서버에서 응답한 에러
+        console.warn('🚨 Server Response:', error.response.data);
+        return {
+          status: error.response.status,
+          error: error.response.data?.error || '서버 오류가 발생했습니다.',
+          data: error.response.data?.data,
+        };
+      } else if (error.request) {
+        // 요청이 전송되었지만 응답이 없음
+        return {
+          status: 503,
+          error: '서버 응답이 없습니다. 네트워크 상태를 확인하세요.',
+          data: 'null',
+        };
+      }
+    }
 
-//     // Axios 외의 일반적인 예외 처리
-//     console.warn('❌ Unexpected Error:', error);
-//     return {
-//       status: 500,
-//       error: '알 수 없는 오류가 발생했습니다.',
-//       data: 'null',
-//     };
-//   },
-//   // 응답 데이터 가공
-// );
+    // Axios 외의 일반적인 예외 처리
+    console.warn('❌ Unexpected Error:', error);
+    return {
+      status: 500,
+      error: '알 수 없는 오류가 발생했습니다.',
+      data: 'null',
+    };
+  },
+  // 응답 데이터 가공
+);
 
 export default sessionClient;
