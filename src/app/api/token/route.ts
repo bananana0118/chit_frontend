df867a3ac123b18ef627cdf6fc076ccea7338426
bananana0118 <bananana0118@gmail.com>
2025-04-25 17:19:27 +0900

    [feat]: pr-agent test

--- src/app/api/token/route.ts
+++ src/app/api/token/route.ts
@@ -1,7 +1,7 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 
-export async function POST(req: NextRequest): Promise<Response> {
+export async function POST(): Promise<Response> {
   const response = NextResponse.json({ shouldRefresh: true }, { status: 200 });
-
+  console.log('dasdasdasd');
   return response;
 }
