265b158d4bb68349f8cc855f736d40fe1c38df97
bananana0118 <bananana0118@gmail.com>
2025-04-26 22:53:39 +0900

    [feat]: layout 컴포넌트 조절하기, NavBar 조정

--- src/app/[channelId]/[sessionCode]/layout.tsx
+++ src/app/[channelId]/[sessionCode]/layout.tsx
@@ -1,10 +1,14 @@
-'use client';
 import CommonLayout from '@/components/layout/CommonLayout';
+import { Suspense } from 'react';
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <CommonLayout>{children}</CommonLayout>;
+  return (
+    <CommonLayout>
+      <Suspense>{children}</Suspense>
+    </CommonLayout>
+  );
 }
--- src/app/streamer/layout.tsx
+++ src/app/streamer/layout.tsx
@@ -1,13 +1,5 @@
-import CommonLayout from '@/components/layout/CommonLayout';
-import NavBar from '@/components/layout/NavBar';
-
 type Props = { children: React.ReactNode };
 
 export default function RootLayout({ children }: Props) {
-  return (
-    <CommonLayout>
-      <NavBar></NavBar>
-      {children}
-    </CommonLayout>
-  );
+  return { children };
 }
